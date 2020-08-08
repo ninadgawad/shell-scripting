057ed1c86b890ff43a918b9ad9b36111484dda41
Ninad Gawad <ninadgawad@gmail.com>
2020-08-07 22:15:14 -0400

    Create 01_Start.sh

---
+++ 01_Start.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# Author : Ninad Gawad
+# Basic Shell Script 
+
+# echo is used to print
+echo "Hello World"
+echo "Hello Shell"
+
+echo "Do you like Shell?"
+read ANSWER
+echo "You Said:, $ANSWER"
+
+
+
+## Run this using command $./01_Start.sh
