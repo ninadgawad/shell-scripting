50cb56a62e6f02723793edbbcd34a90f759f8008
Ninad Gawad <ninadgawad@gmail.com>
2021-04-11 14:42:10 -0400

    Update 02_Helper.sh

--- 02_Helper.sh
+++ 02_Helper.sh
@@ -22,14 +22,21 @@ grep -i <keyword" *.log
 ## Filer Log file to show column  1 ^ 4
 awk '{print $1,$4}' apache.log
 
-# Show top 10 lines in a file
+## Show top 10 lines in a file
 head -10 /tmp/test.logs
 
 
-# Show Last 10 lines in a file
+## Find the current user 
+id
+
+## Opeb VI editor for a file data.csv
+vi data.csv
+
+
+## Show Last 10 lines in a file
 tail -10 /tmp/test.logs
 
-# Show MD5 hash for a file 
+## Show MD5 hash for a file 
 md5sum file.dat
 46798r57fca45c46a84b7419f8b740123
 
